9440f837df561e12097444d274f8cd034521a90d
ckue-opportunity <ckue.opportunity@gmail.com>
2023-03-02 17:00:46 +0100

    component EventEntry

--- src/App-static.vue
+++ src/App-static.vue
@@ -4,16 +4,15 @@
     <span class="site-description">{{ currentDate }}</span>
 
     <!-- entry list -->
-    <!-- v-if will only be shown if there are entries available -->
     <ul v-if="entries" class="entry-list">
-      <li class="entry-item" v-for="entry in entries" :key="entry.id">
-        <span class="entry-daytime">{{entry[0]}} Uhr, {{entry[1].replaceAll("/", ".")}}</span><br />
-        <h3 class="entry-title">{{entry[2]}}</h3>
-        <span class="entry-description">{{entry[3]}}</span><br />
+      <li v-for="entry in entries" :key="entry.id" class="entry-item">
+        <span class="entry-daytime">{{ entry[1] }} {{ entry[0] }} Uhr</span><br />
+        <h3 class="entry-title">{{ entry[2] }}</h3>
+        <span class="entry-description">{{ entry[3] }}</span><br />
       </li>
     </ul>
 
-    <!-- ELSE  -->
+    <!-- default text is only shown if the list of entries is null or undefined -->
     <h1 v-else>Keine Events zur Zeit vorhanden!</h1>
 
     <!-- footer -->
@@ -35,43 +34,28 @@
 </template>
 
 <script>
-import axios from "axios"; // axios is a library for making HTTP requests to the backend
-
 export default {
   name: "App",
   data() {
     return {
       title: "Welcome to Opportunity",
-      sheet_id: "1CR1UKN0LAPNs6lWbfA2gBI2FazmWdVSFIzIwi5TG5Z4",
-      api_token: "AIzaSyA-qeDXOhEeQDA0vQf7LgkF7DQtGnAtmAU",
-      entries: [],
       currentDate: "",
+      entries: []
     };
-  },
-  computed: {
-    // computed properties are like data properties, but with a method combined and it gets executed automatically, instead of calling a function explicitly
-    gsheet_url() {
-      return `https://sheets.googleapis.com/v4/spreadsheets/${this.sheet_id}/values:batchGet?ranges=A2%3AE100&valueRenderOption=FORMATTED_VALUE&key=${this.api_token}`;
-    },
   },
   methods: {
     getData() {
-      axios.get(this.gsheet_url).then((response) => {
-        this.entries = response.data.valueRanges[0].values;
-      });
-    },
-    /*
-    getData() {
       this.entries = [
-        ["8:25", "event 0", "title 0", "description 0"],
-        ["17:25", "event 1", "title 1", "description 1"]
+        // Time, Date, Title, Description
+        ["17:00",	"08/09/2022",	"Abschlussfeier",	"Erfolgreich vorbei!!!"],
+        ["19:00",	"09/09/2022",	"Aufräumen",	"Bitte alle helfen"],
+        ["19:30",	"10/09/2022",	"Ausschlafen",	"Gute Nacht..."]
       ];
     },
-    */
     updateCurrentDate() {
       let today = new Date();
-      const currentDate = `${today.getDate()}.${today.getMonth() + 1}.${today.getFullYear()}`;
-      this.currentDate = currentDate;
+      this.counter++;
+      this.currentDate = `${today.getDate()}.${today.getMonth() + 1}.${today.getFullYear()}`;
     },
     refreshData() {
       this.updateCurrentDate();
@@ -80,8 +64,8 @@ export default {
   },
   mounted() {
     this.refreshData(); // get first initial data and then wait for the next
-    setInterval(this.refreshData, 18000000); // wait 30mins for next update (1000 * 60 * 30)
-  },
+    setInterval(this.refreshData, 60000); // wait one minute for next update (1000 * 60)
+  }
 };
 </script>
 
---
+++ src/components/EventEntry.vue
@@ -0,0 +1,16 @@
+<template>
+    <span class="entry-daytime">{{entry[0]}} Uhr, {{entry[1].replaceAll("/", ".")}}</span><br />
+    <h3 class="entry-title">{{entry[2]}}</h3>
+    <span class="entry-description">{{entry[3]}}</span><br />
+</template>
+
+<script>
+export default {
+  props: ['entry'],
+  data() {
+    return {
+      counter: 0
+    }
+  }
+}
+</script>
